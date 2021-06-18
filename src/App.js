0c4f135d5312d9570e3c6be60ce690536ec05437
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-06-18 13:02:45 -0500

    roughed logout button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import axiosWithAuth from "./helpers/axiosWithAuth";
 import "./styles.scss";
 
 function App() {
+  const logout = () => {
+    const token = localStorage.getItem('token')
+    axiosWithAuth()
+      .post('/api/logout')
+      .then(res => {
+        localStorage.removeItem('token')
+        window.location.href = '/login'
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a data-testid="logoutButton" onClick={logout} href="#">logout</a>
         </header> 
 
         <Route exact path="/" component={Login} />
